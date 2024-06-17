fce4679da6fcf3563643d8dd205d33325f18f57b
Thanos Tsiantoulis <Thanos.Tsiantoulis@coxautoinc.com>
2024-06-17 12:49:50 +0200

    adding angular front end

---
+++ src/test/java/com/example/apimatrurity/ApimatrurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apimatrurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimatrurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

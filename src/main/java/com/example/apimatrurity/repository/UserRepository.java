4d3e588459a49f832194645c52bf77294cd45443
Thanos Tsiantoulis <Thanos.Tsiantoulis@coxautoinc.com>
2024-06-21 13:28:53 +0200

    Sring Boot: first commit with a basic skeleton

--- src/main/java/com/example/apimatrurity/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.myproject.repository;
-
-import com.example.myproject.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}

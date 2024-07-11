36ef2644b092e265ba8c24d8f773058c511d137d
Thanos Tsiantoulis <Thanos.Tsiantoulis@coxautoinc.com>
2024-07-11 16:44:48 +0200

    Authentication with JWT is successfull

--- src/main/java/com/example/apimaturity/security/JwtUtils.java
+++ src/main/java/com/example/apimaturity/security/JwtUtils.java
@@ -1,44 +1,63 @@
 package com.example.apimaturity.security;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+
 @Component
 public class JwtUtils {
-    private String secretKey = "yourSecretKey"; // Ensure this key is strong and unique
+    
+    private SecretKey jwtSecret;
 
     @Value("${app.jwtSecret}")
-    private String jwtSecret;
+     public void setJwtSecret(String secretKeyString) {
+        // Assuming the secret is Base64 encoded
+        byte[] decodedKey = Base64.getDecoder().decode(secretKeyString);
+        // Specify the algorithm you intend to use for the JWT
+        this.jwtSecret = new SecretKeySpec(decodedKey, 0, decodedKey.length, "HmacSHA256");
+    }
+
+    
+    @Value("${app.jwtSecret}")
+    private String Secret;   
 
     @Value("${app.jwtExpirationMs}")
     private int jwtExpirationMs;
 
     public String generateJwtToken(Authentication authentication) {
-        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+        //UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+        Date now = new Date();
+        Date expiDate = new Date(now.getTime() + jwtExpirationMs);
+
         return Jwts.builder()
-                .setSubject(userPrincipal.getUsername())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes()), SignatureAlgorithm.HS512)
+                //.header()
+                .subject(authentication.getName())
+                .issuedAt(now)
+                .expiration(expiDate)
+                .signWith(jwtSecret)
                 .compact();
     }
 
     public boolean validateJwtToken(String token) {
         try {
-            Claims claims = Jwts.parserBuilder()
-                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+            //basic validation not parsing Claims
+            Jwts.parser()
+                    .verifyWith(jwtSecret)
                     .build()
-                    .parseClaimsJws(token)
-                    .getBody();
+                    .parseSignedClaims(token)
+                    .getPayload();
+                    
             return true;
-        } catch (SignatureException e) {
+        } catch (JwtException e) {
             System.out.println("Invalid JWT signature: " + e.getMessage());
         } catch (Exception e) {
             System.out.println("Token validation error: " + e.getMessage());
@@ -47,11 +66,11 @@ public class JwtUtils {
     }
 
     public String getUserNameFromJwtToken(String token) {
-        Claims claims = Jwts.parserBuilder()
-                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+        Claims claims = Jwts.parser()
+                .verifyWith(jwtSecret)
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
         return claims.getSubject();
     }
 }
